0b822c07436284f43b8534475549f9edf862ed98
coreyarch1234 <harrilal@mit.edu>
2017-09-29 15:12:47 -0700

    reset project from word-cloud

--- earth-view/src/App.js
+++ earth-view/src/App.js
@@ -1,21 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Search from './search'
+var axios = require('axios'); //to make Ajax requests
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            imageURL: null
+        }
+    }
+
+    findSatelliteImage(address){
+        //geocode address and get {lat, lon}
+        const location = {
+            lat: address.lat,
+            lon: address.lon
+        }
+        const query = 'https://api.astrodigital.com/v2.0/search/?' + 'contains=' + location.lat + ',' + location.lon;
+
+        console.log(query);
+        axios.get(query)
+        .then((response) => {
+            console.log(response);
+            this.setState({imageURL: response.data.results[0].thumbnail});
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    showImage(){
+        if (this.state.imageURL === null){
+            return <div></div>;
+        }else{
+            return <img src={this.state.imageURL}/>;
+        }
+    }
+
+    componentWillMount() {
+    }
+
+    render() {
+        return(
+            <div>
+            <Search
+                placeholder="Enter Address"
+                label = "Search"
+                onSubmit={(address) => {
+                    console.log("address: ", address);
+                    const addressArr = address.split(",");
+                    console.log(addressArr);
+                    address = {
+                        lat: addressArr[0],
+                        lon: addressArr[1]
+                    }
+                    this.findSatelliteImage(address);
+            }}/>
+            {this.showImage()}
+            </div>
+        );
+    }
+
 }
 
 export default App;
---
+++ earth-view/src/search.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+
+class Search extends Component{
+    constructor(props){
+        super(props); //for props to get set on this object
+
+        this.state = {
+            searchTerm: ""
+        }
+    }
+
+    render(){
+        return (
+            <div>
+            <form onSubmit={(e) =>{
+                e.preventDefault(); //so page does not refresh after submission
+                this.props.onSubmit(this.state.searchTerm);
+            }} style={styles.form}>
+                <input
+                    type="text"
+                    placeholder = {this.props.placeholder}
+                    value = {this.state.searchTerm}
+                    onChange={(e) => { //What you are typing in
+                        this.setState({
+                            searchTerm: e.target.value
+                        })
+                    }} style={styles.input}/>
+                <button type="submit">{this.props.label}</button>
+            </form>
+            </div>
+        );
+    }
+}
+
+
+const styles = {
+      form: {
+         display: 'flex',
+         justifyContent: 'center'
+     },
+     input: {
+         color: '#2d4'
+     }
+}
+
+export default Search;
